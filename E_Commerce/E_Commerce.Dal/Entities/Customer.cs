63e96f6a7cc5c0d2646cddaad5a24f4a1bf76e25
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-22 21:04:23 +0500

    Add CartProduct entity with properties to Dal.Entities folder

--- E_Commerce/E_Commerce.Dal/Entities/Customer.cs
+++ E_Commerce/E_Commerce.Dal/Entities/Customer.cs
@@ -7,7 +7,7 @@ public class Customer
     public string LastName { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    //public List<Cart> Carts { get; set; }
+    public List<Cart> Carts { get; set; }
     //public List<Order> Orders { get; set; }
     //public List<Card> Cards { get; set; }
 }
--- E_Commerce/E_Commerce.Dal/Entities/Product.cs
+++ E_Commerce/E_Commerce.Dal/Entities/Product.cs
@@ -9,6 +9,6 @@ public class Product
     public decimal Price { get; set; }
     public int StockQuantity { get; set; }
     public string? ImageLink { get; set; }
-    //public List<CartProduct> CartProducts { get; set; }
+    public List<CartProduct> CartProducts { get; set; }
     //public List<OrderProduct> OrderProducts { get; set; }
 }
