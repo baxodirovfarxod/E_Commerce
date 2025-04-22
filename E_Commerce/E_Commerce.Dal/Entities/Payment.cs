aa977d9744195ac0cf54698040e1b23cb357aa26
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-22 21:29:05 +0500

    Add Payment entity with properties to Dal.Entities folder

---
+++ E_Commerce/E_Commerce.Dal/Entities/Payment.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce.Dal.Entities;
+
+public class Payment
+{
+    public long PaymentId { get; set; }
+    public long OrderId { get; set; }
+    public string PaymentMethod { get; set; }
+    public string PaymentStatus { get; set; }
+    public decimal PaidAmount { get; set; }
+    public DateTime PaidAt { get; set; }
+    // public Order Order { get; set; }
+}
+
