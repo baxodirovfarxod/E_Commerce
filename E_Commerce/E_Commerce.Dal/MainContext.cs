13a7d12d5798bb238557db17e2125100fff2bbf8
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-28 01:37:09 +0500

    Register all Fluent API configurations in MainContext

--- E_Commerce/E_Commerce.Dal/MainContext.cs
+++ E_Commerce/E_Commerce.Dal/MainContext.cs
@@ -22,7 +22,22 @@ public class MainContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Customer>().ToTable(nameof(Customer));
+        modelBuilder.Entity<Product>().ToTable(nameof(Product));
+        modelBuilder.Entity<Cart>().ToTable(nameof(Cart));
+        modelBuilder.Entity<Order>().ToTable(nameof(Order));
+        modelBuilder.Entity<Payment>().ToTable(nameof(Payment));
+        modelBuilder.Entity<Card>().ToTable(nameof(Card));
 
+        modelBuilder.Entity<CartProduct>().ToTable(nameof(CartProduct));
+        modelBuilder.Entity<CartProduct>()
+            .HasKey(cp => new { cp.CartId, cp.ProductId });
+
+
+        modelBuilder.Entity<OrderProduct>().ToTable(nameof(OrderProduct));
+        modelBuilder.Entity<OrderProduct>()
+            .HasKey(op => new { op.OrderId, op.ProductId });
     }
 
 }
