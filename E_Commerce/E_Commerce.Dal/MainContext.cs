4c6c8c6e9350aa815ab53c1572ee236d26920852
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-27 18:09:01 +0500

    Add MainContext class with OnModelCreating configuration loader

---
+++ E_Commerce/E_Commerce.Dal/MainContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Dal;
+
+public class MainContext : DbContext
+{
+    public MainContext(DbContextOptions<MainContext> options)
+         : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+
+    // DbSet'lar keyinchalik qo‘shiladi
+}
+
+
