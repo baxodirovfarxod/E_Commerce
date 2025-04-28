1f5f8685084fc273b1c78dd7f097267400374b66
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-04-29 00:24:59 +0500

    Add repository interfaces for all entities in separate folders

---
+++ E_Commerce/E_Commerce.Repository/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Dal.Entities;
+
+namespace E_Commerce.Repository.Repositories;
+
+public interface IProductRepository
+{
+    Task<long> InsertProductAsync(Product product);
+    Task<Product> SelectProductByIdAsync(long productId);
+    Task UpdateProductAsync(Product product);
+}
