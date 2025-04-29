17acef0adeb2edd98e287386f43d6ac0b5329f50
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-29 14:39:54 +0500

    Create FluentValidation for CardCreateDto and CardUpdateDto

--- E_Commerce/E_Commerce.Bll/Validators/CardValidators/CardCreateDtoValidator.cs
+++ E_Commerce/E_Commerce.Bll/Validators/CardValidators/CardCreateDtoValidator.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using E_Commerce.Bll.Dtos.CardDTOs;
+using FluentValidation;
 
-//namespace E_Commerce.Bll.Validators.CardValidators;
-//{
-//   public class CardCreateDto
-//    {
-//    }
-//}
+namespace E_Commerce.Bll.Validators.CardValidators;
+
+public class CardCreateDtoValidator : AbstractValidator<CardCreateDto>
+{
+    public CardCreateDtoValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .GreaterThan(0).WithMessage("Customer ID must be greater than 0.");
+
+        RuleFor(x => x.CardNumber)
+            .NotEmpty().WithMessage("Card number is required.")
+            .Matches(@"^\d{16}$").WithMessage("Card number must be 16 digits.")
+            .CreditCard().WithMessage("Invalid card number format.");
+
+        RuleFor(x => x.HolderName)
+            .NotEmpty().WithMessage("Holder name is required.");
+
+        RuleFor(x => x.ExpirationMonth)
+            .InclusiveBetween(1, 12)
+            .WithMessage("Expiration month must be between 1 and 12.");
+
+        RuleFor(x => x.ExpirationYear)
+            .GreaterThan(DateTime.Now.Year)
+            .WithMessage("Expiration year must be greater than the current year.");
+
+        RuleFor(x => x.Cvv)
+            .InclusiveBetween(100, 999).WithMessage("CVV must be a 3-digit number.");
+    }
+}
+
