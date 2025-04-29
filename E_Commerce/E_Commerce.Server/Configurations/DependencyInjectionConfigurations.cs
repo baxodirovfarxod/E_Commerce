ce0d03cbbaa9915963742b2b80a7c4a1e0f76933
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-04-29 06:45:05 +0500

    Create MappingProfile in BLL and configure AutoMapper in DependencyInjectionConfigurations

---
+++ E_Commerce/E_Commerce.Server/Configurations/DependencyInjectionConfigurations.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Bll.MappingProfile;
+
+namespace E_Commerce.Server.Configurations;
+
+public static class DependencyInjectionConfigurations
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+    }
+}
