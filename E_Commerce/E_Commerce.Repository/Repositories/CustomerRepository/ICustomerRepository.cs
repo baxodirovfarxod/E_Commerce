9adacd462a6c5e41e677ae0515714a1338f55193
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-29 13:26:24 +0500

    Implement ICustomerRepository with full logic

---
+++ E_Commerce/E_Commerce.Repository/Repositories/CustomerRepository/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Dal.Entities;
+
+namespace E_Commerce.Repository.Repositories.CustomerRepository;
+
+public interface ICustomerRepository
+{
+    Task<long> InsertCustomerAsync(Customer customer);
+    Task<Customer?> SelectCustomerByIdAsync(long customerId);
+    Task DeleteCustomerByIdAsync(long customerId);
+    Task<ICollection<Customer>> SelectAllCustomersAsync(int skip, int take);
+}
