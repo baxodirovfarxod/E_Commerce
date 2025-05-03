8bd708d61000ac399972397e179ced133bb45913
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-05-03 07:49:58 +0500

    Optimize

--- E_Commerce/E_Commerce.Bll/Dtos/ProductDTOs/ProductUpdateDto.cs
+++ E_Commerce/E_Commerce.Bll/Dtos/ProductDTOs/ProductUpdateDto.cs
@@ -1,11 +1,7 @@
 ﻿namespace E_Commerce.Bll.Dtos.ProductDTOs;
 
-public class ProductUpdateDto
+public class ProductUpdateDto : ProductCreateDto
 {
     public long ProductId { get; set; }
-    public string? Name { get; set; }
-    public decimal Price { get; set; }
-    public int StockQuantity { get; set; }
-    public string? ImageLink { get; set; }
 }
 
